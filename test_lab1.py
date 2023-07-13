d3e01c4c91463439c7f46df092249bffb9041c92
giorgi-injgia <giorgi.injgia.1@btu.edu.ge>
2023-07-14 01:51:26 +0400

    good tests

---
+++ test_lab1.py
@@ -0,0 +1,11 @@
+import pytest
+from example import add_numbers
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+
+def test_add_numbers_negative():
+    assert add_numbers(-5, 10) == 5
+
+def test_add_numbers_zero():
+    assert add_numbers(0, 0) == 0
